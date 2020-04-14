cc625d446d09e8406fbcc6b1c75a47b52fdea12c
Andrew Baird <baird.andrew4@gmail.com>
2020-04-14 14:10:00 -0500

    Have a placeholder for an image and the details for a person's details. Working to use a function component next

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component{
-  render() {
-    return <div className="header">{this.props.title}</div>
-  }
+/*
+	const testData = [
+			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+      {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+    ];
+*/
+
+const CardList = (props) => (
+	<div>
+		<Card />
+	</div>
+);
+
+class Card extends React.Component {
+	render() {
+		return (
+			<div className="github-profile">
+				<img src="https://placehold.it/75" alt="" />
+				<div className="info">
+					<div className="name">Name here...</div>
+					<div className="company">Company here...</div>
+				</div>
+			</div>
+		);
+	}
+}
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<div className="header">{this.props.title}</div>
+				<Card />
+			</div>
+		);
+	}
 }
 
 export default App;
